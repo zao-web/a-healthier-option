4f56c07d96703bed7ba70a24f037982d9142795e
Justin Sternberg <justin@dsgnwrks.pro>
2017-04-13 22:21:07 -0400

    Add a print_r view for the option

--- a-healthier-option.php
+++ a-healthier-option.php
@@ -665,7 +665,7 @@ class AHO_Options_List_Table extends WP_List_Table {
 			$actions['delete'] = sprintf( '<a href="%s" class="submitdelete" data-id="%d" title="%s">%s</a>', $url, $item->option_name, __( 'Delete this option', 'a-healthier-option' ), __( 'Delete', 'a-healthier-option' ) );
 		}
 
-        return sprintf( '<a href="%1$s"><strong>%2$s</strong></a> %3$s', admin_url( 'tools.php?page=a_healthier_option&action=view&id=' . $item->option_id ), $item->option_name, $this->row_actions( $actions ) );
+        return sprintf( '<a href="%1$s"><strong>%2$s</strong></a> %3$s', admin_url( 'tools.php?page=a_healthier_option&action=view&id=' . $item->option_name ), $item->option_name, $this->row_actions( $actions ) );
     }
 
     /**
@@ -726,6 +726,12 @@ function aho_process_actions() {
 
 	if ( ! isset( $_REQUEST['action'] ) ) {
 		return;
+	}
+
+	if ( isset( $_GET['action'] ) && 'view' === $_GET['action'] && isset( $_GET['id'] ) ) {
+		$name = sanitize_text_field( $_GET['id'] );
+		$css  = '<style type="text/css">body#error-page {margin: 1.5em auto;padding: .5em 2em 1em;max-width: 90%;}</style>';
+		wp_die( $css . '<h1>Option: '. $name .'</h1><xmp>'. print_r( get_option( $name ) , true ) .'</xmp>', $name );
 	}
 
 	if ( isset( $_GET['action'] ) && 'delete' === $_GET['action'] && isset( $_GET['id'] ) ) {
